c82c3bd6924689b609690c72d986f6dc9c5a19df
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2021-03-28 19:44:48 -0400

    add reset css

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 
+// default css to reset styles
+import "./styles/default.css";
+
 Vue.config.productionTip = false;
 
 new Vue({

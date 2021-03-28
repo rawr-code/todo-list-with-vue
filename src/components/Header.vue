714e0530f57b6f47950a3f8d1e034588aa909398
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2021-03-28 19:45:59 -0400

    add components

---
+++ src/components/Header.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <v-card
+      color="grey lighten-4"
+      flat
+      tile
+      style="position: relative; z-index: 100"
+    >
+      <v-toolbar>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+          <v-icon v-if="!drawer">mdi-menu</v-icon>
+          <v-icon v-else>mdi-close</v-icon>
+        </v-app-bar-nav-icon>
+        <router-link to="/">
+          <v-toolbar-title class="font-weight-black black--text">
+            My Task List
+          </v-toolbar-title>
+        </router-link>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-badge color="pink" content="3" overlap>
+            <v-icon>mdi-bell</v-icon>
+          </v-badge>
+        </v-btn>
+
+        <v-btn icon>
+          <v-avatar size="36">
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          </v-avatar>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      hide-overlay
+      class="pt-16"
+    >
+      <v-list nav dense class="pa-0">
+        <h2 class="text-h6 font-weight-black mx-6 my-4">Collections</h2>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-blue--text text--accent-4"
+        >
+          <router-link
+            :to="'/' + nawItem.type"
+            v-for="nawItem in navItems"
+            :key="nawItem.title"
+          >
+            <v-list-item class="py-2 pl-6 mb-0">
+              <v-avatar
+                tile
+                :color="nawItem.color"
+                size="32"
+                class="rounded-lg mr-5"
+              >
+                <v-icon dark size="20"> mdi-{{ nawItem.icon }} </v-icon>
+              </v-avatar>
+              <v-list-item-title>
+                <h2 class="text-body-1 font-weight-black">
+                  {{ nawItem.title }}
+                </h2>
+              </v-list-item-title>
+            </v-list-item>
+          </router-link>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+
+export default Vue.extend({
+  data: () => ({
+    drawer: false,
+    group: null,
+    navItems: [
+      {
+        icon: "grid",
+        title: "All tasks",
+        color: "blue",
+        type: "",
+      },
+      {
+        icon: "account",
+        title: "Personal",
+        color: "yellow",
+        type: "Personal",
+      },
+      {
+        icon: "home",
+        title: "School",
+        color: "red",
+        type: "School",
+      },
+      {
+        icon: "card",
+        title: "Work",
+        color: "brown",
+        type: "Work",
+      },
+    ],
+  }),
+});
+</script>
+
+<style scoped>
+* {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card
+    class="pa-4 d-flex mb-4 todoItem flex-grow-1"
+    elevation="4"
+    @click="click"
+  >
+    <v-radio-group v-model="status" readonly hide-details="true">
+      <v-radio color="blue" value="true" readonly></v-radio>
+    </v-radio-group>
+    <div class="d-flex flex-column flex-grow-1">
+      <p class="text-body-2 mb-0 black--text font-weight-medium">
+        {{ name }}
+      </p>
+      <p class="text-caption mb-0 font-italic">{{ description }}</p>
+    </div>
+    <v-icon v-if="type === 'Personal'" class="mr-1" size="24">
+      mdi-account
+    </v-icon>
+    <v-icon v-else-if="type === 'School'" class="mr-1" size="24">
+      mdi-home
+    </v-icon>
+    <v-icon v-else-if="type === 'Work'" class="mr-1" size="24">mdi-card</v-icon>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+
+export default Vue.extend({
+  name: "TodoItem",
+  props: {
+    name: String,
+    description: String,
+    type: String,
+    status: String,
+    click: Function,
+  },
+});
+</script>
+
+<style>
+.todoItem {
+  border-radius: 1rem !important;
+}
+.todoItem :hover {
+  cursor: pointer;
+}
+
+.todoItem .v-input {
+  padding: 0;
+  margin: 0;
+  margin-right: 0.25rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.todoItem .v-input .v-input__control .v-input__slot {
+  padding: 0 !important;
+}
+</style>
